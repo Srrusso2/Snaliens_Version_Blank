32168b373fdc8eefbe57be221289179aa10cc281
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2023-10-29 01:40:15 -0400

    Movement Code

---
+++ Assets/Scripts/SnailMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailMovement : MonoBehaviour
+{
+    //components
+    public CharacterController CC;
+    public Transform CamTransform;
+
+    //floats
+    public float MoveSpeed;
+    public float MouseSensitivity;
+    private float camRotation = 0f;
+
+    //bools
+    public bool gameActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //will use this later when menu overlays are a thing
+        gameActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameActive)
+        {
+            Vector3 movement = Vector3.zero;
+
+            // X/Z movement
+            float forwardMovement = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+            float sideMovement = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+
+            movement += (transform.forward * forwardMovement) + (transform.right * sideMovement);
+
+            //cam movement
+            float mouseInputY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+            camRotation -= mouseInputY;
+            camRotation = Mathf.Clamp(camRotation, -90f, 90f);
+            CamTransform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0f, 0f));
+
+            float mouseInputX = Input.GetAxis("Mouse X") * MouseSensitivity;
+            transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0f, mouseInputX, 0f));
+
+            CC.Move(movement);
+        }
+    }
+}
