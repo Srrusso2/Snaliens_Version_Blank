1a12b307032482d19db632e380544801e1143f61
zacharymarlowe101 <54627826+zacharymarlowe101@users.noreply.github.com>
2023-10-29 23:20:40 -0400

    Snail Death

--- Assets/Scripts/GroundEnemyMovement.cs
+++ Assets/Scripts/GroundEnemyMovement.cs
@@ -14,20 +14,26 @@ public class GroundEnemyMovement : MonoBehaviour{
     }
 
     void Update(){
-        RaycastHit hit;
-        // making it so that when a raycast is cast forward from the enemy a code checks if it has contacted the player through use of GetComponent<playerScript> 
-        //(or something like that) but it does that in a certain range and if it finds an object with that script it goes directly toawrds it
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)){
-            Debug.DrawLine(transform.position + transform.TransformDirection(Vector3.forward), hit.point, Color.cyan);
-			if(hit.collider.GetComponent<SnailMovement>()==null||hit.distance>enemySightRange){
-                enemySpeed=enemySpeedReg;
-				transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
-                transform.Rotate(0.0f, enemyRotation, 0.0f, Space.Self);
-			}else{
-                enemySpeed=enemySpeedHungry;
-                transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
+        if(gm.gameActive==true){
+            RaycastHit hit;
+            // making it so that when a raycast is cast forward from the enemy a code checks if it has contacted the player through use of GetComponent<playerScript> 
+            //(or something like that) but it does that in a certain range and if it finds an object with that script it goes directly toawrds it
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)){
+                Debug.DrawLine(transform.position + transform.TransformDirection(Vector3.forward), hit.point, Color.cyan);
+                if(hit.collider.GetComponent<SnailMovement>()==null||hit.distance>enemySightRange){
+                    enemySpeed=enemySpeedReg;
+                    transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
+                    transform.Rotate(0.0f, enemyRotation, 0.0f, Space.Self);
+                }else{
+                    enemySpeed=enemySpeedHungry;
+                    transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
+                }
             }
-		}
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider){
+        Destroy(collider.gameObject);
     }
 
 }
