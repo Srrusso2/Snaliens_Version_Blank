77c857f0f62ab9d2d7c380aa8f4ca2cd59cc466d
zacharymarlowe101 <54627826+zacharymarlowe101@users.noreply.github.com>
2023-11-05 12:12:39 -0500

    Various Small Adjustments
    Also added two scenes for the title screen and the cutscene before the game starts, should be working properly once we start to build the game

---
+++ Assets/Scripts/eventSystems.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class eventSystems : MonoBehaviour{
+    public UnityEvent onStart;
+    public UnityEvent onUpdate;
+    public UnityEvent onTriggerEnter;
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
