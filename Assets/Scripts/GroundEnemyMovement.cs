dfa8d5f5c386d2b72b059058f857e8187c7e727b
zacharymarlowe101 <54627826+zacharymarlowe101@users.noreply.github.com>
2023-11-13 13:40:44 -0500

    Enemy Code Adj. and new Scene for Testing

--- Assets/Scripts/GroundEnemyMovement.cs
+++ Assets/Scripts/GroundEnemyMovement.cs
@@ -9,7 +9,7 @@ public class GroundEnemyMovement : MonoBehaviour{
     public float enemyRotation;
     public float enemySightRange; //range of raycast for detecting snails
     public GAME_MANAGER gm;
-    public Vector3 maxSnailienEatSize;
+    public int maxSnailienEatLevel;
     public bool canWormAttack=true;
     public Vector3 enemyStartPos;
     public bool hasAttacked=false;
@@ -25,18 +25,8 @@ public class GroundEnemyMovement : MonoBehaviour{
             //(or something like that) but it does that in a certain range and if it finds an object with that script it goes directly toawrds it
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)){
                 Debug.DrawLine(transform.position + transform.TransformDirection(Vector3.forward), hit.point, Color.cyan);
-                if(hit.collider.GetComponent<SnailMovement>()==null||hit.distance>enemySightRange||gm.snailienManager.snailienHiding||gm.snailienManager.transform.localScale.y>maxSnailienEatSize.y){
-                    if(hasAttacked==true){
-                        transform.position = Vector3.MoveTowards(transform.position, enemyStartPos, enemySpeed*Time.deltaTime);
-                        if(gameObject.transform.position==enemyStartPos){
-                            hasAttacked=false;
-                        }
-                    }else{
-                        enemySpeed=enemySpeedReg;
-                        transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
-                        transform.Rotate(0.0f, enemyRotation, 0.0f, Space.Self);
-                        //canWormAttack=false;
-                    }
+                if(hit.collider.GetComponent<SnailMovement>()==null||hit.distance>enemySightRange||gm.snailienManager.snailienHiding||gm.snailienManager.level>=maxSnailienEatLevel){
+                    transform.position = Vector3.MoveTowards(transform.position, enemyStartPos, enemySpeed*Time.deltaTime);
                 }else{
                     enemySpeed=enemySpeedHungry;
                     transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
@@ -46,7 +36,7 @@ public class GroundEnemyMovement : MonoBehaviour{
                     hasAttacked=true;
                 }
             }
-            if(gm.snailienManager.transform.localScale.y>maxSnailienEatSize.y){
+            if(gm.snailienManager.level>maxSnailienEatLevel){
                 transform.gameObject.tag = "SnailFood";
                 gm.snailienManager.snailFood = GameObject.FindGameObjectsWithTag("SnailFood");
             }
